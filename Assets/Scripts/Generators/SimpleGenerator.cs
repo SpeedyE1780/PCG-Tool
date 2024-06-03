f170b9f9f9b98330b80861ae7a84d7e404e79356
Sandro Saab <sandroesaab@gmail.com>
2024-06-03 15:55:30 +0300

    Add Generator2D

--- Assets/Scripts/Generators/SimpleGenerator.cs
+++ Assets/Scripts/Generators/SimpleGenerator.cs
@@ -33,23 +33,6 @@ public class SimpleGenerator : Generator
         };
     }
 
-    private void SpawnCell(GameObject cell, Vector3 position)
-    {
-        GameObject go = null;
-
-        if (PrefabUtility.IsPartOfAnyPrefab(cell))
-        {
-            go = PrefabUtility.InstantiatePrefab(cell) as GameObject;
-            go.transform.position = position;
-        }
-        else
-        {
-            go = Instantiate(cell, position, Quaternion.identity);
-        }
-
-        Undo.RegisterCreatedObjectUndo(go, "Spawned cell");
-    }
-
     public override void Generate(in GeneratorData data)
     {
         Vector3 direction = GetDirection();
