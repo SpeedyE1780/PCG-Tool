86ed91246d832913b66abed6dda83f7700f7d27d
Sandro Saab <sandroesaab@gmail.com>
2024-06-03 15:27:58 +0300

    Keep prefab link and record object creation in history

--- Assets/Scripts/SimpleGenerator.cs
+++ Assets/Scripts/SimpleGenerator.cs
@@ -1,14 +1,32 @@
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SimpleGenerator", menuName = "Scriptable Objects/PCG/SimpleGenerator")]
 public class SimpleGenerator : ScriptableObject
 {
+    private void SpawnCell(GameObject cell, Vector3 position)
+    {
+        GameObject go = null;
+
+        if (PrefabUtility.IsPartOfAnyPrefab(cell))
+        {
+            go = PrefabUtility.InstantiatePrefab(cell) as GameObject;
+            go.transform.position = position;
+        }
+        else
+        {
+            go = Instantiate(cell, position, Quaternion.identity);
+        }
+
+        Undo.RegisterCreatedObjectUndo(go, "Spawned cell");
+    }
+
     public void Generate(in GeneratorData data)
     {
         for (int i = 0; i < data.limit; i++)
         {
             Vector3 position = data.startPosition + data.size * i * Vector3.forward;
-            Instantiate(data.cell, position, Quaternion.identity);
+            SpawnCell(data.cell, position);
         }
     }
 }
