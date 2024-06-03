3715a24888aaa586921eafa425e09df48b4ebfcd
Sandro Saab <sandroesaab@gmail.com>
2024-06-03 14:44:11 +0300

    Add GeneratorData class

---
+++ Assets/Scripts/GeneratorData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class GeneratorData
+{
+    public readonly GameObject cell;
+    public readonly uint limit;
+    public readonly float size;
+    public readonly Vector3 startPosition;
+}
