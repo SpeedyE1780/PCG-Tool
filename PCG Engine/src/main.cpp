4d924062fa0b9cf6203463e81f4ba7714f6e1712
Sandro Saab <sandroesaab@gmail.com>
2024-06-09 17:07:34 +0300

    Log added point instead of storing and logging later

--- PCG Engine/src/main.cpp
+++ PCG Engine/src/main.cpp
@@ -6,13 +6,11 @@
 using namespace pcg::engine::core;
 using namespace pcg::engine::math;
 
-std::vector<Vector3> points{};
-
 namespace
 {
     void addPoints(Vector3 point)
     {
-        points.push_back(point);
+        std::cout << "Point:" << point.x << "," << point.y << "," << point.z << std::endl;
     }
 
     void logMessage(const std::string& message)
@@ -28,28 +26,11 @@ int main()
     GenerationData data{ 10, 1, { 0, 0, 0 } };
     simpleGeneration(&data, Axis::x, Direction::negative, addPoints);
 
-    for (auto& p : points)
-    {
-        std::cout << "P:" << p.x << "," << p.y << "," << p.z << std::endl;
-    }
-
-    points.clear();
     std::cout << "2D Generation" << std::endl;
     generation2D(&data, Plane::yz, true, addPoints);
 
-    for (auto& p : points)
-    {
-        std::cout << "P:" << p.x << "," << p.y << "," << p.z << std::endl;
-    }
-
-    points.clear();
     std::cout << "3D Generation" << std::endl;
     generation3D(&data, true, addPoints);
-
-    for (auto& p : points)
-    {
-        std::cout << "P:" << p.x << "," << p.y << "," << p.z << std::endl;
-    }
 
     return 0;
 }
