9ce0a80d445ee66374b99eaf01d5e50a5f7a5771
Sandro Saab <sandroesaab@gmail.com>
2024-06-11 23:34:36 +0100

    Move multidimensional generation to MultiDimensionalGeneration

--- PCG Engine/core/src/api.cpp
+++ PCG Engine/core/src/api.cpp
@@ -1,6 +1,6 @@
 #include <pcg/engine/core/api.hpp>
-
 #include <pcg/engine/core/SimpleGeneration.hpp>
+#include <pcg/engine/core/MultiDimensionalGeneration.hpp>
 
 namespace pcg::engine::core
 {
@@ -22,5 +22,34 @@ namespace pcg::engine::core
     void generation1D(GenerationData* data, math::Axis axis, math::Direction direction, addPointCallback callback)
     {
         simpleGeneration(data, axis, direction, callback);
+    }
+
+    void generation2D(GenerationData* data, math::Plane plane, bool disableOverlap, addPointCallback callback)
+    {
+        std::vector<const math::Vector3*> directions = getPlaneUnitVectors(plane);
+
+        if (directions.empty())
+        {
+            return;
+        }
+
+        multiDimensionalGeneration(data, directions, disableOverlap, callback);
+    }
+
+    void generation3D(GenerationData* data, bool disableOverlap, addPointCallback callback)
+    {
+        static const std::vector<const math::Vector3*> directions
+        {
+            {
+                &math::Vector3::right,
+                &math::Vector3::left,
+                &math::Vector3::up,
+                &math::Vector3::down,
+                &math::Vector3::forward,
+                &math::Vector3::backward
+            }
+        };
+
+        multiDimensionalGeneration(data, directions, disableOverlap, callback);
     }
 }
