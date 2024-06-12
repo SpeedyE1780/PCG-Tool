ca405af72f04bc28d8397915b2db7cafb1b15140
Sandro Saab <sandroesaab@gmail.com>
2024-06-12 18:42:19 +0100

    Add Maze file

---
+++ PCG Engine/core/include/pcg/engine/core/Maze.hpp
@@ -0,0 +1,8 @@
+#ifndef PCG_ENGINE_CORE_MAZE_HPP
+#define PCG_ENGINE_CORE_MAZE_HPP
+
+namespace pcg::engine::core
+{
+}
+
+#endif // PCG_ENGINE_CORE_MAZE_HPP
---
+++ PCG Engine/core/src/Maze.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/core/Maze.hpp>
+
+#include <pcg/engine/math/vector3.hpp>
+
+#include <vector>
+
+namespace pcg::engine::core
+{
+}
