a137af40b6d72521ef8690594a94739259cb1b37
Sandro Saab <sandroesaab@gmail.com>
2024-06-13 21:14:40 +0100

    Add logs to sidewinder

--- PCG Engine/core/src/Maze.cpp
+++ PCG Engine/core/src/Maze.cpp
@@ -323,6 +323,8 @@ namespace pcg::engine::core
 
     void sidewinder(int width, int height, addMazePointCallback callback)
     {
+        utility::logInfo("Sidewinder Maze Generation Started");
+
         std::vector<std::vector<int>> grid(width, std::vector<int>(height, 0));
 
         for (std::size_t h = 0; h < height; ++h)
@@ -333,22 +335,31 @@ namespace pcg::engine::core
             {
                 if (h > 0 && (w + 1 == width || math::Random::generate(0, 2) == 0))
                 {
+                    utility::logInfo("Ending run and carving north");
                     int cell = math::Random::generate(runStart, w + 1);
                     grid[h][cell] |= down;
                     grid[h - 1][cell] |= up;
                     runStart = w + 1;
+                    std::ostringstream oss{};
+                    oss << "Value set at " << cell << "-" << h << "/" << cell << "-" << h - 1;
+                    utility::logInfo(oss.str());
                     callback(cell, h, grid[h][cell]);
                     callback(cell, h - 1, grid[h - 1][cell]);
                 }
                 else if (w + 1 < width)
                 {
+                    utility::logInfo("Carving east");
                     grid[h][w] |= right;
                     grid[h][w + 1] |= left;
-
+                    std::ostringstream oss{};
+                    oss << "Value set at " << w << "-" << h << "/" << w + 1 << "-" << h;
+                    utility::logInfo(oss.str());
                     callback(w, h, grid[h][w]);
                     callback(w + 1, h, grid[h][w + 1]);
                 }
             }
         }
+
+        utility::logInfo("Sidewinder Maze Generation Started");
     }
 }
