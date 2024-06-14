f89fe5d24e5f75c9ffc60ed3e95554dfc0d39acb
Sandro Saab <sandroesaab@gmail.com>
2024-06-14 20:36:25 +0100

    Use callback functor with simple generation

--- PCG Engine/level-generation/include/pcg/engine/level-generation/SimpleGeneration.hpp
+++ PCG Engine/level-generation/include/pcg/engine/level-generation/SimpleGeneration.hpp
@@ -6,69 +6,11 @@
 #include <pcg/engine/math/enums.hpp>
 #include <pcg/engine/math/vector3.hpp>
 
-#include <pcg/engine/utility/logging.hpp>
-
-#include <functional>
+#include <pcg/engine/utility/CallbackFunctor.hpp>
 
 namespace pcg::engine::level_generation
 {
-    namespace
-    {
-        std::function<void(math::Vector3& position, float offset)> getUpdatePositionFunction(math::Axis axis)
-        {
-            switch (axis)
-            {
-            case math::Axis::x:
-            {
-                return [](math::Vector3& position, float offset)
-                    {
-                        position.x += offset;
-                    };
-            }
-            case math::Axis::y:
-            {
-                return [](math::Vector3& position, float offset)
-                    {
-                        position.y += offset;
-                    };
-            }
-            case math::Axis::z:
-            {
-                return [](math::Vector3& position, float offset)
-                    {
-                        position.z += offset;
-                    };
-            }
-            default:
-            {
-                utility::logError("Can't interpret axis returning with no generation");
-                return nullptr;
-            }
-            }
-        }
-    }
-
-    template<typename addPointCallback>
-    void simpleGeneration(GenerationData* data, math::Axis axis, math::Direction direction, addPointCallback callback)
-    {
-        utility::logInfo("Simple Generation Started");
-        std::function<void(math::Vector3& position, float offset)> updatePosition = getUpdatePositionFunction(axis);
-        math::Vector3 position = data->startPoint;
-        const float offset = direction == math::Direction::positive ? data->size : -data->size;
-
-        if (!updatePosition)
-        {
-            return;
-        }
-
-        for (int i = 0; i < data->limit; ++i)
-        {
-            callback(position);
-            updatePosition(position, offset);
-        }
-
-        utility::logInfo("Simple Generation Ended");
-    }
+    void simpleGeneration(GenerationData* data, math::Axis axis, math::Direction direction, utility::CallbackFunctor<math::Vector3>&& callback);
 }
 
 #endif // PCG_ENGINE_LEVEL_GENERATION_SIMPLE_GENERATION_HPP
