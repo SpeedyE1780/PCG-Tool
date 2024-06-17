27e41c763163f8a0a635a9994dea36e854b8b134
Sandro Saab <sandroesaab@gmail.com>
2024-06-17 20:22:40 +0100

    Generate combination with a minimum of element active

--- PCG Engine/combination-generation/include/pcg/engine/combination-generation/CombinationGenerator.hpp
+++ PCG Engine/combination-generation/include/pcg/engine/combination-generation/CombinationGenerator.hpp
@@ -6,6 +6,7 @@
 namespace pcg::engine::combination_generation
 {
     void generateCombination(int elementCount, utility::CallbackFunctor<void(int, bool)>&& callback);
+    void generateCombination(int elementCount, int minElementCount, utility::CallbackFunctor<void(int, bool)>&& callback);
 }
 
 #endif // PCG_ENGINE_COMBINATION_GENERATION_COMBINATION_GENERATOR_HPP
--- PCG Engine/combination-generation/src/CombinationGenerator.cpp
+++ PCG Engine/combination-generation/src/CombinationGenerator.cpp
@@ -2,8 +2,60 @@
 
 #include <pcg/engine/math/random.hpp>
 
+#include <algorithm>>
+#include <numeric>
+#include <random>
+#include <vector>
+
 namespace pcg::engine::combination_generation
 {
+    namespace
+    {
+        constexpr bool isElementActive(int index, int combination)
+        {
+            return ((1 << index) & combination) > 0;
+        }
+
+        constexpr int countActiveBits(int combination)
+        {
+            int activeBits = 0;
+
+            for (int tempCombination = combination; tempCombination > 0; tempCombination >>= 1)
+            {
+                if ((tempCombination & 1) > 0)
+                {
+                    activeBits += 1;
+                }
+            }
+
+            return activeBits;
+        }
+
+        void activateRemainingBits(int& combination, int elementCount, int activeBits, int minimumBits)
+        {
+            std::vector<int> indices(elementCount);
+            std::default_random_engine randomEngine{};
+            std::iota(begin(indices), end(indices), 0);
+            std::shuffle(begin(indices), end(indices), randomEngine);
+
+            for (int elementIndex : indices)
+            {
+                if (isElementActive(elementIndex, combination))
+                {
+                    continue;
+                }
+
+                combination |= 1 << elementIndex;
+                activeBits += 1;
+
+                if (activeBits >= minimumBits)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
     void generateCombination(int elementCount, utility::CallbackFunctor<void(int, bool)>&& callback)
     {
         const int combinationCount = 1 << elementCount;
@@ -11,7 +63,25 @@ namespace pcg::engine::combination_generation
 
         for (int elementIndex = 0; elementIndex < elementCount; ++elementIndex)
         {
-            callback(elementIndex, ((1 << elementIndex) & combination) > 0);
+            callback(elementIndex, isElementActive(elementIndex, combination));
+        }
+    }
+
+    void generateCombination(int elementCount, int minElementCount, utility::CallbackFunctor<void(int, bool)>&& callback)
+    {
+        const int combinationCount = 1 << elementCount;
+        int combination = math::Random::generate(1, combinationCount);
+
+        int activeElements = countActiveBits(combination);
+
+        if (activeElements < minElementCount)
+        {
+            activateRemainingBits(combination, elementCount, activeElements, minElementCount);
+        }
+
+        for (int elementIndex = 0; elementIndex < elementCount; ++elementIndex)
+        {
+            callback(elementIndex, isElementActive(elementIndex, combination));
         }
     }
 }
