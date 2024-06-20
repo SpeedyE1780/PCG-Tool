674e49345b8c8c3c010e9a0cf7402bdfefe37573
Sandro Saab <sandroesaab@gmail.com>
2024-06-20 15:41:41 +0100

    Move classes to separate file

---
+++ Unity/Assets/Scripts/PCGAPI/Classes.cs
@@ -0,0 +1,27 @@
+using System.Runtime.InteropServices;
+
+namespace PCGAPI
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GenerationParameters
+    {
+        public uint count;
+        public float size;
+        public Vector3 startPoint;
+    }
+}
