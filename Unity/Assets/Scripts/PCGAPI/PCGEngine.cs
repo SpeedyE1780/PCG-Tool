29dcdeb5b2e67e3b68447970c70b34961ebe927d
Sandro Saab <sandroesaab@gmail.com>
2024-06-20 17:29:00 +0100

    Add MultiDimensionalGeneration window

--- Unity/Assets/Scripts/PCGAPI/PCGEngine.cs
+++ Unity/Assets/Scripts/PCGAPI/PCGEngine.cs
@@ -12,5 +12,7 @@ namespace PCGAPI
         public static extern void GenerateMaze(int width, int height, bool invokeAfterGeneration, MazeAlgorithm algorithm, AddMazeNode addMazeNode);
         [DllImport("pcg-engine-c-apid", EntryPoint = "simpleGeneration")]
         public static extern void SimpleGeneration(ref GenerationParameters generationParameters, Axis axis, AddNode addNode);
+        [DllImport("pcg-engine-c-apid", EntryPoint = "multiDimensionGeneration")]
+        public static extern void MultiDimensionalGeneration(ref GenerationParameters generationParameters, Axis axis, bool disableOverlap, AddNode addNode);
     }
 }
