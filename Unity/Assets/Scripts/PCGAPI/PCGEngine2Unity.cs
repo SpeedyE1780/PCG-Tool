fa762c05c565de111c86cc01d505a8312fb53d2a
Sandro Saab <sandroesaab@gmail.com>
2024-06-20 16:49:01 +0100

    Delete GeneratorData

--- Unity/Assets/Scripts/PCGAPI/PCGEngine2Unity.cs
+++ Unity/Assets/Scripts/PCGAPI/PCGEngine2Unity.cs
@@ -1,5 +1,3 @@
-using PCGAPI.Generators;
-
 namespace PCGAPI
 {
     public static class PCGEngine2Unity
@@ -12,16 +10,6 @@ namespace PCGAPI
         public static UnityEngine.Vector3 PCGEngineVectorToUnity(Vector3 vector)
         {
             return new UnityEngine.Vector3(vector.x, vector.y, vector.z);
-        }
-
-        public static GenerationParameters GeneratorDataToPCGEngineGenerationParameters(GeneratorData generatorData)
-        {
-            return new GenerationParameters()
-            {
-                count = generatorData.limit,
-                size = generatorData.size,
-                startPoint = UnityToPCGEngineVector(generatorData.startPosition)
-            };
         }
     }
 }
