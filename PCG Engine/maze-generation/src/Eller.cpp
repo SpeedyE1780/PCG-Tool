74c4440d4f2d7575d6fcecb21831a5387ad026de
Sandro Saab <sandroesaab@gmail.com>
2024-06-23 21:16:35 +0100

    Add Eller files

---
+++ PCG Engine/maze-generation/src/Eller.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/maze-generation/Eller.hpp>
+
+namespace pcg::engine::maze_generation
+{
+    void eller(int width, int height, bool invokeAfterGeneration, MazeCallback&& callback)
+    {
+
+    }
+}
