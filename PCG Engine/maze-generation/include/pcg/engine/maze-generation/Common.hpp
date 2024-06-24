ab9ecdca735686b67e6b768014ea719bf1089f28
Sandro Saab <sandroesaab@gmail.com>
2024-06-24 12:35:23 +0100

    Fix generate grid inverting width and height

--- PCG Engine/maze-generation/include/pcg/engine/maze-generation/Common.hpp
+++ PCG Engine/maze-generation/include/pcg/engine/maze-generation/Common.hpp
@@ -30,7 +30,7 @@ namespace pcg::engine::maze_generation
     /// @param height Grid height
     /// @param defaultValue Default value when allocating grid
     /// @return A 2D vector representing the maze's empty grid
-    inline constexpr Grid generateGrid(int width, int height, utility::enums::Direction defaultValue = utility::enums::Direction::none) { return Grid(width, std::vector<utility::enums::Direction>(height, defaultValue)); }
+    inline constexpr Grid generateGrid(int width, int height, utility::enums::Direction defaultValue = utility::enums::Direction::none) { return Grid(height, std::vector<utility::enums::Direction>(width, defaultValue)); }
     /// @brief Get default directions used when generating maze
     /// @return a vector containing { left, right, forward, backward }
     inline constexpr std::vector<utility::enums::Direction> getDefaultDirections()
