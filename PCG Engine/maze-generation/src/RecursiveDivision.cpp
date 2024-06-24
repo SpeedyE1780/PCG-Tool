5a98b14071e54ecad8f4a329dff64236761921be
Sandro Saab <sandroesaab@gmail.com>
2024-06-24 17:37:11 +0100

    Add recursive division files

---
+++ PCG Engine/maze-generation/src/RecursiveDivision.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/maze-generation/RecursiveDivision.hpp>
+
+namespace pcg::engine::maze_generation
+{
+    void recursiveDivision(int width, int height, bool invokeAfterGeneration, MazeCallback&& callback)
+    {
+
+    }
+}
