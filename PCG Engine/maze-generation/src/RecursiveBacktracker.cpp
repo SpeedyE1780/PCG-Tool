33ea3b2d2b590e86895d30483dae50a518089fe3
Sandro Saab <sandroesaab@gmail.com>
2024-06-24 16:37:59 +0100

    Add RecursiveBacktracker files

---
+++ PCG Engine/maze-generation/src/RecursiveBacktracker.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/maze-generation/RecursiveBacktracker.hpp>
+
+namespace pcg::engine::maze_generation
+{
+    void recursiveBacktracker(int width, int height, bool invokeAfterGeneration, MazeCallback&& callback)
+    {
+
+    }
+}
