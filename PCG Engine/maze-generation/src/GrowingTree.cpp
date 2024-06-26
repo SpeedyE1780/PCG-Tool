b489a95a347bd31c286a0c2b4f64b824a2ce6e3e
Sandro Saab <sandroesaab@gmail.com>
2024-06-26 17:52:24 +0100

    Add Growing Tree files

---
+++ PCG Engine/maze-generation/src/GrowingTree.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/maze-generation/GrowingTree.hpp>
+
+namespace pcg::engine::maze_generation
+{
+    void growingTree(int width, int height, bool invokeAfterGeneration, MazeCallback&& callback)
+    {
+
+    }
+}
