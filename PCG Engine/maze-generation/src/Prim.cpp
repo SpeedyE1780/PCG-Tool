1948ee39aa4b00ac89da04e8c1902aaf3a14884f
Sandro Saab <sandroesaab@gmail.com>
2024-06-26 11:33:53 +0100

    Add Prim files

---
+++ PCG Engine/maze-generation/src/Prim.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/maze-generation/Prim.hpp>
+
+namespace pcg::engine::maze_generation
+{
+    void prim(int width, int height, bool invokeAfterGeneration, MazeCallback&& callback)
+    {
+
+    }
+}
