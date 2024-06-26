d96f2e207271e3594964d3e87d91f183c78420a5
Sandro Saab <sandroesaab@gmail.com>
2024-06-26 10:25:54 +0100

    Add Kruskal files

---
+++ PCG Engine/maze-generation/src/Kruskal.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/maze-generation/Kruskal.hpp>
+
+namespace pcg::engine::maze_generation
+{
+    void kruskal(int width, int height, bool invokeAfterGeneration, MazeCallback&& callback)
+    {
+
+    }
+}
