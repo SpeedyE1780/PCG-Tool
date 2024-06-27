80b801e76d8d1b4734eec9918afda556dfaf5576
Sandro Saab <sandroesaab@gmail.com>
2024-06-27 12:44:49 +0100

    Add Blobby Division files

---
+++ PCG Engine/maze-generation/src/BlobbyDivision.cpp
@@ -0,0 +1,9 @@
+#include <pcg/engine/maze-generation/BlobbyDivision.hpp>
+
+namespace pcg::engine::maze_generation
+{
+    void pcg::engine::maze_generation::blobbyDivision(int width, int height, bool invokeAfterGeneration, MazeCallback&& callback)
+    {
+
+    }
+}
