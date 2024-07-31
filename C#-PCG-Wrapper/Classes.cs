20a1df8d9c9bc727a0281815a715914eb096de57
Sandro Saab <sandroesaab@gmail.com>
2024-07-31 18:13:45 +0300

    Add Classes Documentation

--- C#-PCG-Wrapper/Classes.cs
+++ C#-PCG-Wrapper/Classes.cs
@@ -3,13 +3,31 @@ using System.Runtime.InteropServices;
 
 namespace PCGAPI
 {
+    /// <summary>
+    /// Represent a Vector3
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct Vector3
     {
+        /// <summary>
+        /// Value of x axis
+        /// </summary>
         public float x;
+        /// <summary>
+        /// Value of y axis
+        /// </summary>
         public float y;
+        /// <summary>
+        /// Value of z axis
+        /// </summary>
         public float z;
 
+        /// <summary>
+        /// Construct a Vector3
+        /// </summary>
+        /// <param name="x">Value of x axis</param>
+        /// <param name="y">Value of y axis</param>
+        /// <param name="z">Value of z axis</param>
         public Vector3(float x, float y, float z)
         {
             this.x = x;
@@ -18,17 +36,38 @@ namespace PCGAPI
         }
     }
 
+    /// <summary>
+    /// Parameters used to generate levels
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct GenerationParameters
     {
+        /// <summary>
+        /// Number of nodes that will need to be spawned
+        /// </summary>
         public uint count;
+        /// <summary>
+        /// Size of spawned node
+        /// </summary>
         public float size;
+        /// <summary>
+        /// Position of first spawned node
+        /// </summary>
         public Vector3 startPoint;
     }
 
+    /// <summary>
+    /// Interface used to represent a node in a sequence
+    /// </summary>
     public interface ISequenceNode
     {
+        /// <summary>
+        /// IEnumerable of possible next nodes
+        /// </summary>
         IEnumerable<ISequenceNode> NextNodes { get; }
+        /// <summary>
+        /// Number of possible next node (Usually NextNodes.Count())
+        /// </summary>
         int NextCount { get; }
     }
 }
