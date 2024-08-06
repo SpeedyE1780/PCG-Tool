116188cefec1af9fad4e137b2c08c1a6761d616b
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-06 14:36:41 +0300

    Add utility documentation

--- Python/pcg/utility.py
+++ Python/pcg/utility.py
@@ -1,12 +1,21 @@
 import ctypes
 from collections.abc import Callable
 
-pcgDLL = ctypes.CDLL('./pcg-engine-c-api.dll')
+pcgDLL = ctypes.CDLL("./pcg-engine-c-api.dll")
 
 logCallback = ctypes.CFUNCTYPE(ctypes.c_void_p, ctypes.c_char_p)
 logFunction = None
+"""Function used to log pcg engine messages"""
 
-def setLoggingFunction(callback : Callable[[str], None]):
+
+def setLoggingFunction(callback: Callable[[str], None]):
+    """
+    Set the function used to log messages from the pcg engine to python
+
+    Args:
+        callback (Callable[[str], None]): Function used to log messages
+    """
+
     global logFunction
     logFunction = logCallback(callback)
     pcgDLL.setLoggingFunction(logFunction)
