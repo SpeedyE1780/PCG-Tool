c2956d1b391fb2cb622156ffc2b47ff7d99cdd1b
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-07 16:29:25 +0300

    Add grid

--- Python/whack_a_mole.py
+++ Python/whack_a_mole.py
@@ -13,11 +13,26 @@ screen.fill(black)
 
 Running = True
 
+def drawGrid(startX: int, startY :int, gridWidth: int, gridHeight : int, nodeSize : int, offset : int):
+    
+    currentX = startX
+    currentY = startY
+
+    for w in range(gridWidth):
+        for h in range(gridHeight):
+            pygame.draw.circle(screen, white, [currentX, currentY], nodeSize, 3)
+            currentY += nodeSize * 2 + offset
+        currentY = startY
+        currentX += nodeSize * 2 + offset
+    
+
 while Running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 Running = False
+    screen.fill(black)
+    drawGrid(100, 100, 8, 8, 10, 5)
     pygame.display.update()
 
 pygame.quit()
