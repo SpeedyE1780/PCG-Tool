6a422525f3ac1bd89070e31e52a5dd3043502f44
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-07 16:39:58 +0300

    Add moles to grid

--- Python/whack_a_mole.py
+++ Python/whack_a_mole.py
@@ -13,26 +13,65 @@ screen.fill(black)
 
 Running = True
 
-def drawGrid(startX: int, startY :int, gridWidth: int, gridHeight : int, nodeSize : int, offset : int):
-    
+
+class Mole:
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.hasMole = False
+
+    def draw(self):
+        pygame.draw.circle(screen, white, [self.x, self.y], self.radius)
+        if(self.hasMole):
+            pygame.draw.circle(screen, black, [self.x, self.y], self.radius * 0.5)
+        
+    def addMole(self):
+        self.hasMole = True
+
+
+def drawGrid(
+    startX: int,
+    startY: int,
+    gridWidth: int,
+    gridHeight: int,
+    nodeSize: int,
+    offset: int,
+) -> list[Mole]:
     currentX = startX
     currentY = startY
 
-    for w in range(gridWidth):
-        for h in range(gridHeight):
-            pygame.draw.circle(screen, white, [currentX, currentY], nodeSize, 3)
+    moles = []
+
+    for _ in range(gridWidth):
+        for _ in range(gridHeight):
+            moles.append(Mole(currentX, currentY, nodeSize))
             currentY += nodeSize * 2 + offset
+
         currentY = startY
         currentX += nodeSize * 2 + offset
-    
+
+    return moles
+
+moleRemaining = False
+moles = []
 
 while Running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 Running = False
+
     screen.fill(black)
-    drawGrid(100, 100, 8, 8, 10, 5)
+    
+    if not moleRemaining:
+        moles = drawGrid(100, 100, 3, 3, 10, 5)
+        combinations.generateCombinationWithMinimumElements(moles, 1, lambda index, included: moles[index].addMole() if included else None)
+        moleRemaining = True
+
+    for mole in moles:
+        mole.draw()
+
     pygame.display.update()
 
 pygame.quit()
