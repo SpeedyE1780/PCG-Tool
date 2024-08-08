0045f72fc92f3c1d95b0bfba9238922892fad838
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-08 14:48:10 +0300

    Add function to draw node

--- Python/demo/maze.py
+++ Python/demo/maze.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.locals
 from pcg import mazes
 
 pygame.init()
@@ -12,6 +13,19 @@ screen.fill(black)
 
 Running = True
 
+
+def drawNode(x, y, length, up, down, left, right):
+    if up:
+        pygame.draw.line(screen, white, (x, y), (x + length, y))
+    if down:
+        pygame.draw.line(screen, white, (x, y + length), (x + length, y + length))
+
+    if left:
+        pygame.draw.line(screen, white, (x, y), (x, y + length))
+    if right:
+        pygame.draw.line(screen, white, (x + length, y), (x + length, y + length))
+
+
 while Running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -19,6 +33,7 @@ while Running:
                 Running = False
 
     screen.fill(black)
+    drawNode(20, 20, 20, True, True, True, True)
     pygame.display.update()
 
 pygame.quit()
