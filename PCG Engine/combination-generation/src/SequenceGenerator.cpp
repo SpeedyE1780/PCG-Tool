a63fe26c085ef9aea21254ca0718921d5f746761
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-12 10:59:01 +0300

    Add sequence generation function

--- PCG Engine/combination-generation/src/SequenceGenerator.cpp
+++ PCG Engine/combination-generation/src/SequenceGenerator.cpp
@@ -7,7 +7,7 @@ namespace pcg::engine::combination_generation
     namespace
     {
         /// @brief Generate a sequence starting from node
-        /// @param node Last node added to sequence
+        /// @param node First node added to sequence
         void generateSequence(ISequenceNode* node)
         {
             while (node->getNextCount() > 0)
@@ -16,10 +16,34 @@ namespace pcg::engine::combination_generation
                 node = node->getNext();
             }
         }
+
+        /// @brief Generate a sequence starting from node with a max number of nodes
+        /// @param node First node in sequence
+        /// @param count Max number of node in sequence
+        std::vector<ISequenceNode*> generateSequence(ISequenceNode* node, int count)
+        {
+            std::vector<ISequenceNode*> sequence{};
+            sequence.reserve(count);
+            sequence.push_back(node);
+
+            while (count > 0 && node->getNextCount() > 0)
+            {
+                node = node->getNextAt(math::Random::number(node->getNextCount()));
+                sequence.push_back(node);
+                count--;
+            }
+
+            return sequence;
+        }
     }
 
     void generateSequence(ISequenceNode& node)
     {
         generateSequence(&node);
+    }
+
+    std::vector<ISequenceNode*> generateSequence(ISequenceNode& node, int count)
+    {
+        return generateSequence(&node, count);
     }
 }
