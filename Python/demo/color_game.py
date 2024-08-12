a10fb44f3a19aefc3717f1211b9b827a07230084
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-12 14:08:46 +0300

    Fill current circle in sequence

--- Python/demo/color_game.py
+++ Python/demo/color_game.py
@@ -19,9 +19,30 @@ screen.fill(black)
 Running = True
 clock = pygame.time.Clock()
 fps = 60
+nextTime = 0.5
+time = nextTime
+currentNodeIndex = 0
+
+sequence = []
+nodes = [red, green, blue, yellow]
+
+node = combinations.SequenceNode()
+node.addNextNodes(nodes)
+
+
+def updateSequence(index):
+    if index == -1:
+        return 0
+
+    sequence.append(nodes[index])
+    return len(nodes[index])
+
+
+combinations.generateCyclicSequence(node, 3, updateSequence)
 
 while Running:
     deltaTime = clock.tick(fps)
+    time -= deltaTime * 0.001
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -30,12 +51,26 @@ while Running:
 
     screen.fill(black)
 
-    pygame.draw.circle(screen, red, [600, 320], 20,  1)
-    pygame.draw.circle(screen, green, [560, 360], 20,  1)
-    pygame.draw.circle(screen, blue, [640, 360], 20,  1)
-    pygame.draw.circle(screen, yellow, [600, 400], 20,  1)
+    pygame.draw.circle(
+        screen, red, [600, 320], 20, 0 if sequence[currentNodeIndex] == red else 1
+    )
+    pygame.draw.circle(
+        screen, green, [560, 360], 20, 0 if sequence[currentNodeIndex] == green else 1
+    )
+    pygame.draw.circle(
+        screen, blue, [640, 360], 20, 0 if sequence[currentNodeIndex] == blue else 1
+    )
+    pygame.draw.circle(
+        screen, yellow, [600, 400], 20, 0 if sequence[currentNodeIndex] == yellow else 1
+    )
 
     pygame.display.update()
 
+    if time <= 0:
+        time = nextTime
+        currentNodeIndex += 1
+
+        if currentNodeIndex >= len(sequence):
+            Running = False
 
 pygame.quit()
