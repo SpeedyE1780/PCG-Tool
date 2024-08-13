3d5c117b0a8abe62c31194f4081bc6d01dc6639b
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-13 15:19:22 +0300

    Document WFCNode

--- Unity/Assets/Scripts/PCGAPI/Demo/WFCNode.cs
+++ Unity/Assets/Scripts/PCGAPI/Demo/WFCNode.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 
 namespace PCGAPI.Demo
 {
+    /// <summary>
+    /// Class representing a node in levels generated using wave function collapse
+    /// </summary>
     public class WFCNode : MonoBehaviour, IWFCNode
     {
-        [SerializeField]
+        [SerializeField, Tooltip("Right Wall")]
         private GameObject right;
-        [SerializeField]
+        [SerializeField, Tooltip("Left Wall")]
         private GameObject left;
-        [SerializeField]
+        [SerializeField, Tooltip("Forward Wall")]
         private GameObject forward;
-        [SerializeField]
+        [SerializeField, Tooltip("Backward Wall")]
         private GameObject backward;
-        [SerializeField]
+        [SerializeField, Tooltip("Upward Stair")]
         private GameObject up;
-        [SerializeField]
+        [SerializeField, Tooltip("Downward Stair")]
         private GameObject down;
 
         private static bool HasNeighbour(LevelGenerationDirection current, LevelGenerationDirection direction)
@@ -22,6 +25,10 @@ namespace PCGAPI.Demo
             return (current & direction) > 0;
         }
 
+        /// <summary>
+        /// Toggle node's Wall/Stair
+        /// </summary>
+        /// <param name="adjacentNodes">Adjacent Nodes bit mask</param>
         public void SetAdjacentNodes(LevelGenerationDirection adjacentNodes)
         {
             right.SetActive(!HasNeighbour(adjacentNodes, LevelGenerationDirection.right));
