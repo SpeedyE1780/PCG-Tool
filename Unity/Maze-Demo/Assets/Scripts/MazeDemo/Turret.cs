90d40ff2ff1d74af0376c5e44303c1bd0ca59cd1
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-14 12:47:34 +0300

    Add projectile

--- Unity/Maze-Demo/Assets/Scripts/MazeDemo/Turret.cs
+++ Unity/Maze-Demo/Assets/Scripts/MazeDemo/Turret.cs
@@ -12,15 +12,26 @@ namespace MazeDemo
         private float rotationSpeed = 30f;
         [SerializeField]
         private LayerMask mask;
+        [SerializeField]
+        private Projectile projectile;
+        [SerializeField]
+        private float cooldown = 0.4f;
 
         private Transform target;
         private readonly Collider[] targetCollider = new Collider[1];
+        private float currentCooldown = 0f;
 
         private void Update()
         {
+            currentCooldown -= Time.deltaTime;
+
             if (Physics.OverlapSphereNonAlloc(transform.position, range, targetCollider, mask) > 0)
             {
                 target = targetCollider[0].transform;
+            }
+            else
+            {
+                target = null;
             }
 
             if (target != null)
@@ -29,6 +40,12 @@ namespace MazeDemo
                 forward.y = 0f;
 
                 headTransform.rotation = Quaternion.RotateTowards(headTransform.rotation, Quaternion.LookRotation(forward, Vector3.up), rotationSpeed * Time.deltaTime);
+
+                if (Vector3.Angle(headTransform.forward, forward) < 5f && currentCooldown < 0)
+                {
+                    currentCooldown = cooldown;
+                    Instantiate(projectile, headTransform.position, headTransform.rotation);
+                }
             }
         }
 
