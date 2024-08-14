8f654fe20a0b7508575e570f4670ac3848122614
SpeedyE1780 <sandroesaab@gmail.com>
2024-08-14 11:56:16 +0300

    Add player movement

---
+++ Unity/Maze-Demo/Assets/Scripts/MazeDemo/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace MazeDemo
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody rb;
+        [SerializeField]
+        private float speed;
+
+        private Vector3 velocity = new Vector3();
+
+        void Update()
+        {
+            velocity.x = Input.GetAxis("Horizontal");
+            velocity.y = 0;
+            velocity.z = Input.GetAxis("Vertical");
+
+            velocity.Normalize();
+            velocity *= speed;
+            velocity.y = rb.linearVelocity.y;
+        }
+
+        private void FixedUpdate()
+        {
+            rb.linearVelocity = velocity;
+        }
+    }
+}
