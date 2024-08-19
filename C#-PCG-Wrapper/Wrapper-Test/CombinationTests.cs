de8713b2c379bd05bdfd3cf91e6617d12b470b7a
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-08-19 23:18:22 +0300

    Add combination tests

--- C#-PCG-Wrapper/Wrapper-Test/CombinationTests.cs
+++ C#-PCG-Wrapper/Wrapper-Test/CombinationTests.cs
@@ -29,5 +29,41 @@ namespace PCGAPI.Tests
 
             Assert.Equal(expected, combination);
         }
+
+        [Fact]
+        public void CombinationWith7Elements()
+        {
+            ResetSeed();
+            var nums = GetNums();
+            List<int> combination = [];
+            List<int> expected = new() { 1, 2, 3, 4, 5, 6, 8 };
+            PCGEngine.GenerateCombination(nums.Length, 7, (index, included) =>
+                {
+                    if (included)
+                    {
+                        combination.Add(nums[index]);
+                    }
+                });
+
+            Assert.Equal(expected, combination);
+        }
+
+        [Fact]
+        public void CombinationWith1And3And10Active()
+        {
+            ResetSeed();
+            var nums = GetNums();
+            List<int> combination = [];
+            List<int> expected = new() { 1, 2, 3, 6, 10 };
+            PCGEngine.GenerateCombination(nums.Length, [0, 2, 9], 3, (index, included) =>
+                {
+                    if (included)
+                    {
+                        combination.Add(nums[index]);
+                    }
+                });
+
+            Assert.Equal(expected, combination);
+        }
     }
 }
