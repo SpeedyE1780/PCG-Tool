41b27d0a272ce7b77c8c721b8065dddd738f0bf5
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-08-20 12:33:08 +0300

    Add WaveFunction Collapse Test

--- C#-PCG-Wrapper/Wrapper-Test/PCGTest.cs
+++ C#-PCG-Wrapper/Wrapper-Test/PCGTest.cs
@@ -12,5 +12,13 @@
             var coordinates = line.Split(' ');
             return new Vector3(float.Parse(coordinates[0]), float.Parse(coordinates[1]), float.Parse(coordinates[2]));
         }
+
+        public static Tuple<Vector3, LevelGenerationDirection> ConvertLineToWFCData(string line)
+        {
+            var wfcData = line.Split(' ');
+            Vector3 position = new(float.Parse(wfcData[0]), float.Parse(wfcData[1]), float.Parse(wfcData[2]));
+            LevelGenerationDirection direction = (LevelGenerationDirection)int.Parse(wfcData[3]);
+            return new Tuple<Vector3, LevelGenerationDirection>(position, direction);
+        }
     }
 }
---
+++ C#-PCG-Wrapper/Wrapper-Test/WaveFunctionCollapse.cs
@@ -0,0 +1,39 @@
+﻿namespace PCGAPI.Tests
+{
+    public class WaveFunctionCollapse : PCGTest
+    {
+        private void WaveFunctionCollapseTest(ExpansionMode mode, string filePath)
+        {
+            var positions = File.ReadAllLines(filePath);
+            int index = 0;
+
+            GenerationParameters parameters = new GenerationParameters()
+            {
+                count = 200,
+                size = 1
+            };
+
+            PCGEngine.WaveFunctionCollapseGeneration(ref parameters, mode, Axis.xyz, (position, adjacent) =>
+            {
+                Tuple<Vector3, LevelGenerationDirection> expected = ConvertLineToWFCData(positions[index]);
+                index++;
+                Assert.Equal(expected.Item1, position);
+                Assert.Equal(expected.Item2, adjacent);
+            });
+        }
+
+        [Fact]
+        public void BFS()
+        {
+            string filePath = "GoldenValues/WaveFunctionCollapse/BFS.txt";
+            WaveFunctionCollapseTest(ExpansionMode.BFS, filePath);
+        }
+
+        [Fact]
+        public void DFS()
+        {
+            string filePath = "GoldenValues/WaveFunctionCollapse/DFS.txt";
+            WaveFunctionCollapseTest(ExpansionMode.DFS, filePath);
+        }
+    }
+}
