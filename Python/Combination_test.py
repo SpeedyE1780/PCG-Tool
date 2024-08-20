8ccd2ca25ec6284b945f2ed7421a40647de63a98
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-08-20 15:28:46 +0300

    Add combination tests

---
+++ Python/Combination_test.py
@@ -0,0 +1,46 @@
+import pytest
+from pcg import math, combinations
+
+
+@pytest.fixture
+def resetSeed():
+    math.setSeed(0)
+
+
+@pytest.fixture
+def nums():
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+@pytest.fixture
+def combination():
+    return []
+
+
+def test_simpleCombination(resetSeed, nums, combination):
+    expected = [1, 2, 3, 6]
+    combinations.generateCombination(
+        nums,
+        lambda index, included: combination.append(nums[index]) if included else None,
+    )
+    assert expected == combination
+
+
+def test_combinationWith7Elements(resetSeed, nums, combination):
+    expected = [1, 2, 3, 4, 5, 6, 8]
+    combinations.generateCombinationWithMinimumElements(
+        nums,
+        7,
+        lambda index, included: combination.append(nums[index]) if included else None,
+    )
+    assert expected == combination
+
+
+def test_combinationWith1And3And10Active(resetSeed, nums, combination):
+    expected = [1, 2, 3, 6, 10]
+    combinations.generateCombinationWithIncludedElements(
+        nums,
+        [1, 3, 10],
+        lambda index, included: combination.append(nums[index]) if included else None,
+    )
+    assert expected == combination
