322fa591485062da5771256a73015d93be7b9dfd
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-08-23 11:55:15 +0300

    Use GenerationParameters with Wave Function Collapse

--- Web-App/pages/waveFunctionCollapse.js
+++ Web-App/pages/waveFunctionCollapse.js
@@ -1,27 +1,23 @@
 import * as pc from "playcanvas";
 import { SpawnWFCLevel } from "../components/playcanvas/spawnLevelNode";
 import Viewer from "../components/playcanvas/viewer";
+import GenerationParameters from "../components/levelGeneration/generationParameters";
 
 export default function WaveFunctionCollapse() {
-  let count = 0;
-  let size = 1;
-  let xPosition = 0;
-  let yPosition = 0;
-  let zPosition = 0;
+  let params = {};
   let expansion = 0;
-  let axis = 0;
 
   async function generateLevel() {
     const generationParameters = {
-      nodeCount: count,
-      nodeSize: size,
+      nodeCount: params.count,
+      nodeSize: params.size,
       startPosition: {
-        x: xPosition,
-        y: yPosition,
-        z: zPosition,
+        x: params.xPosition,
+        y: params.yPosition,
+        z: params.zPosition,
       },
+      axes: params.axis,
       expansionMode: expansion,
-      axes: axis,
     };
     console.log(generationParameters);
 
@@ -45,7 +41,7 @@ export default function WaveFunctionCollapse() {
         .json()
         .then((body) => {
           console.log(body);
-          SpawnWFCLevel(body, size);
+          SpawnWFCLevel(body, generationParameters.nodeSize);
         })
         .catch((err) => console.log(err));
     } else {
@@ -56,33 +52,8 @@ export default function WaveFunctionCollapse() {
   return (
     <Viewer>
       <h1>Wave Function Collapse Generation</h1>
-      <input
-        placeholder="Node Count"
-        onChange={(event) => (count = event.target.value)}
-      ></input>
-      <input
-        placeholder="Node Size"
-        onChange={(event) => (size = event.target.value)}
-      ></input>
-      <input
-        placeholder="Start Position X"
-        onChange={(event) => (xPosition = event.target.value)}
-      ></input>
-      <input
-        placeholder="Start Position Y"
-        onChange={(event) => (yPosition = event.target.value)}
-      ></input>
-      <input
-        placeholder="Start Position Z"
-        onChange={(event) => (zPosition = event.target.value)}
-      ></input>
-      <input
-        type="number"
-        placeholder="Axis"
-        onChange={(event) => {
-          axis = event.target.valueAsNumber;
-        }}
-      ></input>
+      <GenerationParameters config={params}></GenerationParameters>
+      <label>Expansion Mode</label>
       <select onChange={(event) => (expansion = event.target.selectedIndex)}>
         <option value="BFS">Breadth First Search</option>
         <option value="DFS">Depth First Search</option>
