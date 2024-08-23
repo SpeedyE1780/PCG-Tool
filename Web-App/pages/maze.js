539992eed34f56da0bd5e181064a288c20310e3e
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-08-23 10:07:35 +0300

    Sort maze algorithm by alphabetical order

--- Web-App/pages/maze.js
+++ Web-App/pages/maze.js
@@ -54,22 +54,10 @@ export default function Maze() {
         onChange={(event) => (selectedAlgorithm = event.target.selectedIndex)}
       >
         <option value="aldousBroder">Aldous Broder</option>
-        <option value="wilson">Wilson</option>
         <option value="binaryTreeNE">Binary Tree NE</option>
         <option value="binaryTreeNW">Binary Tree NW</option>
         <option value="binaryTreeSE">Binary Tree SE</option>
         <option value="binaryTreeSW">Binary Tree SW</option>
-        <option value="sidewinder">Sidewinder</option>
-        <option value="eller">Eller</option>
-        <option value="huntAndKill">Hunt And Kill</option>
-        <option value="recursiveBacktracker">Recursive Backtracker</option>
-        <option value="recursiveDivision">Recursive Division</option>
-        <option value="kruskal">Kruskal</option>
-        <option value="prim">Prim</option>
-        <option value="growingTreeOldest">Growing Tree Oldest</option>
-        <option value="growingTreeMiddle">Growing Tree Middle</option>
-        <option value="growingTreeNewest">Growing Tree Newest</option>
-        <option value="growingTreeRandom">Growing Tree Random</option>
         <option value="blobbyDivisionCorridors">
           Blobby Division Corridors
         </option>
@@ -77,6 +65,18 @@ export default function Maze() {
         <option value="blobbyDivisionMedium">Blobby Division Medium</option>
         <option value="blobbyDivisionLarge">Blobby Division Large</option>
         <option value="blobbyDivisionHuge">Blobby Division Huge</option>
+        <option value="eller">Eller</option>
+        <option value="growingTreeOldest">Growing Tree Oldest</option>
+        <option value="growingTreeMiddle">Growing Tree Middle</option>
+        <option value="growingTreeNewest">Growing Tree Newest</option>
+        <option value="growingTreeRandom">Growing Tree Random</option>
+        <option value="huntAndKill">Hunt And Kill</option>
+        <option value="kruskal">Kruskal</option>
+        <option value="prim">Prim</option>
+        <option value="recursiveBacktracker">Recursive Backtracker</option>
+        <option value="recursiveDivision">Recursive Division</option>
+        <option value="sidewinder">Sidewinder</option>
+        <option value="wilson">Wilson</option>
       </select>
       <button onClick={generateMaze}>Generate</button>
     </Viewer>
