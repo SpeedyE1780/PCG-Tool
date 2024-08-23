6cd990b24a5a9ac6dcc72d48f1ab21f318fdb9c2
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-08-23 11:04:28 +0300

    Use GenerationParameters with SimpleGeneration

--- Web-App/pages/simpleGeneration.js
+++ Web-App/pages/simpleGeneration.js
@@ -1,25 +1,21 @@
 import * as pc from "playcanvas";
 import { SpawnLevel } from "../components/playcanvas/spawnLevelNode";
 import Viewer from "../components/playcanvas/viewer";
+import GenerationParameters from "../components/levelGeneration/generationParameters";
 
 export default function SimpleGeneration() {
-  let count = 0;
-  let size = 1;
-  let xPosition = 0;
-  let yPosition = 0;
-  let zPosition = 0;
-  let axis = 0;
+  let params = {};
 
   async function generateLevel() {
     const generationParameters = {
-      nodeCount: count,
-      nodeSize: size,
+      nodeCount: params.count,
+      nodeSize: params.size,
       startPosition: {
-        x: xPosition,
-        y: yPosition,
-        z: zPosition,
+        x: params.xPosition,
+        y: params.yPosition,
+        z: params.zPosition,
       },
-      axes: axis,
+      axes: params.axis,
     };
     console.log(generationParameters);
 
@@ -43,7 +39,7 @@ export default function SimpleGeneration() {
         .json()
         .then((body) => {
           console.log(body);
-          SpawnLevel(body, size);
+          SpawnLevel(body, generationParameters.nodeSize);
         })
         .catch((err) => console.log(err));
     } else {
@@ -54,33 +50,7 @@ export default function SimpleGeneration() {
   return (
     <Viewer>
       <h1>Simple Generation</h1>
-      <input
-        placeholder="Node Count"
-        onChange={(event) => (count = event.target.value)}
-      ></input>
-      <input
-        placeholder="Node Size"
-        onChange={(event) => (size = event.target.value)}
-      ></input>
-      <input
-        placeholder="Start Position X"
-        onChange={(event) => (xPosition = event.target.value)}
-      ></input>
-      <input
-        placeholder="Start Position Y"
-        onChange={(event) => (yPosition = event.target.value)}
-      ></input>
-      <input
-        placeholder="Start Position Z"
-        onChange={(event) => (zPosition = event.target.value)}
-      ></input>
-      <input
-        type="number"
-        placeholder="Axis"
-        onChange={(event) => {
-          axis = event.target.valueAsNumber;
-        }}
-      ></input>
+      <GenerationParameters config={params}></GenerationParameters>
       <button onClick={generateLevel}>Generate</button>
     </Viewer>
   );
