978762d0553f2829ccedd277800ed4e4d56f53c0
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-08-26 12:14:41 +0300

    Move GridWFC tests to separate file

--- PCG Engine/level-generation/tests/src/WaveFunctionCollapse.cpp
+++ PCG Engine/level-generation/tests/src/WaveFunctionCollapse.cpp
@@ -1,4 +1,3 @@
-#include <pcg/engine/level-generation/GridWaveFunctionCollapse.hpp>
 #include <pcg/engine/level-generation/WaveFunctionCollapse.hpp>
 
 #include <pcg/engine/math/random.hpp>
@@ -62,42 +61,6 @@ namespace pcg::engine::level_generation::tests
                 EXPECT_EQ(x, position.x);
                 EXPECT_EQ(y, position.y);
                 EXPECT_EQ(z, position.z);
-                EXPECT_EQ(static_cast<utility::enums::Direction>(adjacent), adjacentNodes);
-            });
-    }
-
-    TEST_F(WaveFunctionCollapse, Grid2D)
-    {
-        std::ifstream input("GoldenValues/WaveFunctionCollapse/Grid2D.txt");
-
-        waveFunctionCollapse(10, 10, math::Plane::xz, false, [&input](int x, int y, utility::enums::Direction adjacentNodes)
-            {
-                int expectedX = 0;
-                int expectedY = 0;
-                int adjacent = 0;
-
-                input >> expectedX >> expectedY >> adjacent;
-                EXPECT_EQ(expectedX, x);
-                EXPECT_EQ(expectedY, y);
-                EXPECT_EQ(static_cast<utility::enums::Direction>(adjacent), adjacentNodes);
-            });
-    }
-
-    TEST_F(WaveFunctionCollapse, Grid3D)
-    {
-        std::ifstream input("GoldenValues/WaveFunctionCollapse/Grid3D.txt");
-
-        waveFunctionCollapse(10, 10, 10, false, [&input](int x, int y, int z, utility::enums::Direction adjacentNodes)
-            {
-                int expectedX = 0;
-                int expectedY = 0;
-                int expectedZ = 0;
-                int adjacent = 0;
-
-                input >> expectedX >> expectedY >> expectedZ >> adjacent;
-                EXPECT_EQ(expectedX, x);
-                EXPECT_EQ(expectedY, y);
-                EXPECT_EQ(expectedZ, z);
                 EXPECT_EQ(static_cast<utility::enums::Direction>(adjacent), adjacentNodes);
             });
     }
