c392b4623f7131df189936b4698b33c66b8890db
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2024-09-19 16:11:15 +0300

    Add 4D WFC to C# Wrapper

--- C#-PCG-Wrapper/Classes.cs
+++ C#-PCG-Wrapper/Classes.cs
@@ -78,6 +78,86 @@ namespace PCGAPI
     }
 
     /// <summary>
+    /// Represents a Vector4
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vector4
+    {
+        /// <summary>
+        /// Value of x axis
+        /// </summary>
+        public float x;
+        /// <summary>
+        /// Value of y axis
+        /// </summary>
+        public float y;
+        /// <summary>
+        /// Value of z axis
+        /// </summary>
+        public float z;
+        /// <summary>
+        /// Value of w axis
+        /// </summary>
+        public float w;
+
+        /// <summary>
+        /// Construct a Vector3
+        /// </summary>
+        /// <param name="x">Value of x axis</param>
+        /// <param name="y">Value of y axis</param>
+        /// <param name="z">Value of z axis</param>
+        /// <param name="w">Value of w axis</param>
+        public Vector4(float x, float y, float z, float w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        /// <summary>
+        /// Checks for equality between this and other
+        /// </summary>
+        /// <param name="other">Object being compared to this</param>
+        /// <returns>True if this == object</returns>
+        public override bool Equals(object other)
+        {
+            return this == (Vector4)other;
+        }
+
+        /// <summary>
+        /// Returns the instance hash code
+        /// </summary>
+        /// <returns>The instance hash code</returns>
+        public override int GetHashCode()
+        {
+            return (x.GetHashCode() << 0) ^ (y.GetHashCode() << 1) ^ (z.GetHashCode() << 2) ^ (w.GetHashCode() << 3);
+        }
+
+        /// <summary>
+        /// Checks for equality between lhs and rhs
+        /// </summary>
+        /// <param name="lhs">Vector on the left side of ==</param>
+        /// <param name="rhs">Vector on the right side of ==</param>
+        /// <returns>True if lhs == rhs</returns>
+        public static bool operator ==(Vector4 lhs, Vector4 rhs)
+        {
+            return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w;
+        }
+
+        /// <summary>
+        /// Checks for inequality between lhs and rhs
+        /// </summary>
+        /// <param name="lhs">Vector on the left side of !=</param>
+        /// <param name="rhs">Vector on the right side of !=</param>
+        /// <returns>True if lhs != rhs</returns>
+        public static bool operator !=(Vector4 lhs, Vector4 rhs)
+        {
+            return !(lhs == rhs);
+        }
+    }
+
+    /// <summary>
     /// Parameters used to generate levels
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
--- C#-PCG-Wrapper/Wrapper-Test/WaveFunctionCollapse.cs
+++ C#-PCG-Wrapper/Wrapper-Test/WaveFunctionCollapse.cs
@@ -2,22 +2,35 @@
 {
     public class WaveFunctionCollapse : PCGTest
     {
+        static GenerationParameters Parameters => new()
+        {
+            count = 200,
+            size = 1
+        };
+
         private static void WaveFunctionCollapseTest(ExpansionMode mode, string filePath)
         {
             var levelNodes = File.ReadAllLines(filePath);
             int index = 0;
 
-            GenerationParameters parameters = new()
-            {
-                count = 200,
-                size = 1
-            };
-
-            PCGEngine.WaveFunctionCollapseGeneration(parameters, mode, Axis.xyz, (position, adjacent) =>
+            PCGEngine.WaveFunctionCollapseGeneration(Parameters, mode, Axis.xyz, (position, adjacent) =>
             {
                 string expectedLevelNode = levelNodes[index];
                 index++;
                 Assert.Equal(expectedLevelNode, $"{position.x} {position.y} {position.z} {(int)adjacent}");
+            });
+        }
+
+        private static void WaveFunctionCollapse4DTest(ExpansionMode mode, string filePath)
+        {
+            var levelNodes = File.ReadAllLines(filePath);
+            int index = 0;
+
+            PCGEngine.WaveFunctionCollapse4DGeneration(Parameters, mode, Axis.xyz, (position, adjacent) =>
+            {
+                string expectedLevelNode = levelNodes[index];
+                index++;
+                Assert.Equal(expectedLevelNode, $"{position.x} {position.y} {position.z} {position.w} {(int)adjacent}");
             });
         }
 
@@ -33,6 +46,20 @@
         {
             string filePath = "GoldenValues/WaveFunctionCollapse/DFS.txt";
             WaveFunctionCollapseTest(ExpansionMode.DFS, filePath);
+        }
+
+        [Fact]
+        public void BFS4D()
+        {
+            string filePath = "GoldenValues/WaveFunctionCollapse/4DBFS.txt";
+            WaveFunctionCollapse4DTest(ExpansionMode.BFS, filePath);
+        }
+
+        [Fact]
+        public void DFS4D()
+        {
+            string filePath = "GoldenValues/WaveFunctionCollapse/4DDFS.txt";
+            WaveFunctionCollapse4DTest(ExpansionMode.DFS, filePath);
         }
     }
 }
